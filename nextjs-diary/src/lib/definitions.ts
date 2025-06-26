74b1c911dd4032169de504a9029fdffa1218f9f0
HamiAvet <hamov2003@gmail.com>
2025-06-26 18:15:18 +0200

    1

---
+++ nextjs-diary/src/lib/definitions.ts
@@ -0,0 +1,21 @@
+export type User = {
+    id: string,
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string,
+}
+
+export type event = {
+    id: string,
+    title: string,
+    topic: string,
+    categorie: string,
+    day: number,
+    month: string,
+    year: number,
+    week_day: string,
+    hour: number,
+    minuts: number,
+}
+

568f13467fed5578f80eae4cb8dfe279576200da
HamiAvet <hamov2003@gmail.com>
2025-09-17 16:26:31 +0200

    hhc

--- nextjs-diary/app/api/nav/route.js
+++ nextjs-diary/app/api/nav/route.js
@@ -1,12 +1,14 @@
 
 
-/*
+
 export async function GET(request) {
-    const data = await request.json()
+    console.log(request);
+    
+    /*const data = await request.json()
     
     const dbResponse = await getUserByEmail(data)    
     const userRecord = dbResponse[0];
-    return NextResponse.json(events, {status: 200});
+    return NextResponse.json(events, {status: 200});*/
 }
 
-*/
+
